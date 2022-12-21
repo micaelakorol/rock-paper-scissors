a3aee8b15463f0479370a4f798e2f41d4372edd3
micaelakorol <micaelakorol@gmail.com>
2022-12-21 14:31:40 -0300

    refactoing in process

--- src/components/Index.jsx
+++ src/components/Index.jsx
@@ -5,12 +5,25 @@ import scissors from '../images/icon-scissors.svg'
 import '../styles/index.css'
 import logo from '../images/logo.svg'
 
-const Index = ({score,selectIA,user,scoreIA,result}) => {
+const Index = ({score,selectIA,user,scoreIA}) => {
+
+  const game = () => {
+    if(selectIA === 0){
+      return <img src={rock} alt="Rock" /> 
+    }
+    if(selectIA === 1){
+      return <img src={paper} alt="Paper" />
+    }
+    else{
+      return <img src={scissors} alt="Scissors" />
+    }
+    
+  }
 
   return (
   <main id='id-container'>
     <section className='container-score'>
-      <img src={logo} alt="" className='img'/>
+      <img src={logo} alt="logo game" className='img'/>
   {/* ---------------------Tabla de puntaje jugador-------------------------------------*/}
  <section className='table-score'>
     <p className='title-score'>SCORE</p> 
@@ -26,17 +39,15 @@ const Index = ({score,selectIA,user,scoreIA,result}) => {
   
     <section className='section-ia'>
      <section className='result-siri'><h2>Player: Siri </h2></section> 
-      <button>
-        {selectIA === 0 && <img src={rock} alt=''/>}
-          {selectIA === 1 && <img src={paper} alt=''/>}
-            {selectIA === 2 && <img src={scissors} alt=''/>}</button> 
+      <button> {game()}</button> 
+
               </section>
   {/*-------------- Tablero de botones de juego ----------------------------*/}
   <div className='section-game'>
     <h2>Player: You</h2>
-   <button onClick={() => user(0)} className='btn-1'> <img src={rock} alt="" /> </button>
-    <button onClick={() => user(1)} className='btn-2'><img src={paper} alt="" /></button>
-      <button onClick={() => user(2)} className='btn-3'><img src={scissors} alt="" /></button>
+   <button onClick={() => user(0)} className='btn-1'> <img src={rock} alt="rock" /> </button>
+    <button onClick={() => user(1)} className='btn-2'><img src={paper} alt="paper" /></button>
+      <button onClick={() => user(2)} className='btn-3'><img src={scissors} alt="scissors" /></button>
         </div>
           </main>
   )
